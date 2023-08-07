d278a5f93255b6f98f072c17531448a960f7dea6
dariiadrozd <darriiadrozd@gmail.com>
2023-08-07 17:32:32 +0200

    start

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-// На входе строка в виде пароля. 
-// Необходимо написать функцию на проверку, что пароль является надежным 
-// (содержит хотя бы 1 букву в большом регистре, числа, буквы, символ, длина не менее 8 символов)
-
-const str = "Darya_2004";
-
-function doPassword(str) {
-    if (str.length < 8) return "недостаточно символов";
-    let countNum = 0;
-    let countToUpperCase = 0;
-    let countToLowerCase = 0;
-    for (let i = 0; i <= str.length; i++) {
-        if (!isNaN(str[i])) {
-            countNum++
-        } else {
-            str[i].toUpperCase() === str[i] ? countToUpperCase++ : countToLowerCase++;
-        }
-    }
-    if (str.includes('@') || str.includes('-') || str.includes('$') || str.includes('*')) {
-        countSymbol++;
-    }
-    if (countNum === 0 || countToUpperCase === 0 || countToLowerCase == 0 || countSymbol === 0) {
-        return 'ненадежный пароль'
-    } else {
-        return 'надежный пароль'
-    };
-}
-let result = doPassword(str)
-console.log(result);
-
 // Проект
 // Описание проекта:
 // программа генерирует случайное число в диапазоне от 1 до 100 и просит пользователя угадать это число. Если догадка пользователя больше случайного числа, 
@@ -38,4 +8,26 @@ console.log(result);
 // Составляющие проекта: Целые числа; Переменные; Ввод / вывод данных; Условный оператор; Цикл; 
 // Бесконечный цикл; Операторы break, continue; Работа с модулем random для генерации случайных чисел; Функции.
 
-  
+function doRandomNumber() {
+    const num = Math.floor(Math.random() * 100) + 1;
+    let a = +prompt("Программа загадала рандомное число. Ваша зададача угадать его. Введите число от 0 до 100");
+
+    while (true) {
+        if (isNaN(a)) {
+            alert("Пожалуйста, введите число.");
+            a = +prompt("Введите число от 0 до 100");
+            continue;
+        }
+        if (a > num) {
+            a = +prompt(`Вы ввели число ${a}, и это слишком большое значение. Введите число от 0 до ${a}:`);
+        } else if (a < num) {
+            a = +prompt(`Вы ввели число ${a}, и это слишком маленькое значение. Введите число от ${a} до 100:`);
+        } else {
+            alert("Вы угадали! Поздравляем!");
+            break;
+        }
+    }
+}
+
+doRandomNumber();
+
